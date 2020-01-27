b7e2d47c3f349d22872e97d2c9e02578cf7cc010
Christopher Okokon Ntuk <engchris95@gmail.com>
2020-01-27 13:36:24 +0100

    setting up the registration page

---
+++ client/src/components/Register.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <div>
+      <input type="email" name="email" placeholder="Email" v-model="email"> <br><br>
+      <input type="password" name="password" placeholder="Password" v-model="password"> <br><br>
+      <button @click="register">Register</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  name: 'Register',
+  data () {
+    return {
+      email: '1234',
+      password: '1234'
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      alert(response.data.message)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
